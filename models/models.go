74c62b943302d3aefea45c31846333b934a83f16
HajdukSanchez <hajduksanchez.dev@gmail.com>
2023-06-06 07:37:47 -0500

    🚧 [add] initialization new pc

---
+++ models/models.go
@@ -0,0 +1,12 @@
+package models
+
+type Student struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Age  int32  `json:"age"`
+}
+
+type Test struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
